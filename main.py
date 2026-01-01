37d88a194d05f8d61edab945a0240120f7c77110
mycodes21 <mycodes21@gmail.com>
2026-01-01 12:13:26 +0100

    V1.0 izmena gitignore

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import sys  
+import os
+
 from ui.main_window import CypherLensApp
 
 if __name__ == "__main__":
     app = CypherLensApp()
     app.mainloop()
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
